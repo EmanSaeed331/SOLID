93fb3e1c77eb00c9e7262e93173c5d61752bbb89
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-12-29 14:25:22 +0200

    best_case added

---
+++ Single_responsability_principle/best_case.dart
@@ -0,0 +1,25 @@
+class PaymentProcess {
+  void charge(int amount) {
+    //make a charge
+  }
+}
+
+class CreatingReport {
+  String CreateReport() {
+    //create report for payment details
+    return 'CreateReport';
+  }
+}
+
+class PrintingReport {
+  void PrintReport() {
+    //for print report details
+    print('Reportdetails');
+  }
+}
+
+class savingPayment {
+  void SavePayment() {
+    //save payment on DB
+  }
+}
