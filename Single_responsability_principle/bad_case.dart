d2c4b88f262cfc21b1d483a601cc467102405579
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-12-29 14:26:50 +0200

    fix:bad case modified

---
+++ Single_responsability_principle/bad_case.dart
@@ -0,0 +1,18 @@
+class PaymentProcess {
+  void charge(int amount) {
+    //make a charge
+  }
+  String CreateReport() {
+    //create report for payment details
+    return 'CreateReport';
+  }
+
+  void PrintRepost() {
+    //for print report details
+    print('Repostdetails');
+  }
+
+  void SavePayment() {
+    //save payment on DB
+  }
+}
